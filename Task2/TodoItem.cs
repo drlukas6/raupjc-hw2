94fe15a484a6840a3a91e94a94a2b5ef8750ddab
drlukas6 <lsestic@gmail.com>
2017-11-02 23:24:21 +0100

    Second commit. Started Task2

--- Task2/TodoItem.cs
+++ Task2/TodoItem.cs
@@ -35,5 +35,21 @@ namespace Task2
         /// Date entry has been created
         /// </summary>
         public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Constructor for ToDoItem 
+        /// </summary>
+        /// <param name="text">Contents of ToDoItem </param>
+        public TodoItem(string text)
+        {
+            // Generates new unique identifier
+            Id = Guid.NewGuid();
+
+            // DateTime.Now returns local time, it wont always be what you expect (depending where the server is).
+            // We want to use universal (UTC) time which we can easily convert to local when needed.
+            // (usually done in browser on the client side)
+            DateCreated = DateTime.UtcNow;
+            Text = text;
+        }
     }
 }
