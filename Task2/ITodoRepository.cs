41245dfca833fc7c9fb3beed53cb50148e56fda2
drlukas6 <lsestic@gmail.com>
2017-11-03 00:18:28 +0100

    2nd Commit. Overrided comparing methods.

---
+++ Task2/ITodoRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    public interface ITodoRepository
+    {
+        TodoItem Get(Guid todoId);
+
+        TodoItem Add(TodoItem todoItem);
+        
+        bool Remove(Guid todoId);
+        
+        TodoItem Update(TodoItem todoItem);
+        
+        bool MarkAsCompleted(Guid todoId);
+
+        List<TodoItem> GetAll();
+        
+        List<TodoItem> GetActive();
+        
+        List<TodoItem> GetCompleted();
+        
+        List<TodoItem> GetFiltered(Func<TodoItem, bool> filterFunction);
+
+    }
+}
